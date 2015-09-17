c966f5799ce209c533a207ba1ed2c80f527171bf
erik <erik.moqvist@gmail.com>
2015-09-17 08:29:05 +0200

    1.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='xpect',
-      version='1.5.0',
+      version='1.5.1',
       description='Programmed dialogue with interactive streams.',
       long_description=open('README.rst', 'r').read(),
       author='Erik Moqvist',
